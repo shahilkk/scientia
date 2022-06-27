a3404b8a17ca3b06991b66a6fcce3654ee19f374
shahilkk <shahilkhanarimbra@gmail.com>
2022-06-27 16:22:19 -0700

    added course folder and exam and profile

--- student/urls.py
+++ student/urls.py
@@ -6,4 +6,9 @@ app_name='student'
 urlpatterns = [
     path('base',views.base,name='base'),
     path('',views.home,name='home'),
+    path('courses',views.courses,name='courses'),
+    path('classlist',views.classlist,name='classlist'),
+    path('modulwise',views.modulwise,name='modulwise'),
+    path('takexam',views.takexam,name='takexam'),
+    path('profile',views.profile,name='profile'),
 ]
