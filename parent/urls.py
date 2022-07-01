da9fb635996492849533fbb0ecece97d8b6eada7
viveknhc <viveknhc@gmail.com>
2022-07-01 15:08:36 +0530

    new repo

--- parent/urls.py
+++ parent/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('addstudent',views.addstudent,name='addstudent'),
     path('parent-profile',views.parentProfile,name='parent-profile'),
+    path('all-courses',views.allCourses,name='all-courses'),
+    path('aboutcourse',views.aboutCourse,name='aboutcourse'),
 ]
 
