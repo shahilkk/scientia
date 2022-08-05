55982af2d1d9c6b219c391a975bef6d8b0bb9637
shahilkk <shahilkhanarimbra@gmail.com>
2022-08-05 12:09:51 +0530

    conflit message

--- parent/views.py
+++ parent/views.py
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-from .models import *
-
-# Create your views here.
-def base(request):
-    return render(request,"parent/partials/base.html")
-
-
-def index(request):
-    return render(request,"parent/index.html")
-
-
-def addstudent(request):
-    return render(request,"parent/student/addstudent.html")
-
-def parentProfile(request):
-    return render(request,"parent/parent-profile.html")
-
-def category(request):
-    return render(request,"parent/category.html")
-
-def course(request):
-    return render(request,"parent/course.html")
-
-def aboutCourse(request):
-    return render(request,"parent/aboutcourse.html")
-
-def fee(request):
-    return render(request,"parent/fee/pendingfee.html")
-
-def feeDetails(request):
-    return render(request,"parent/fee/feeDetails.html")
-
-def holiday(request):
-    return render(request,"parent/holiday.html")
-
-def studentDetails(request):
-    return render(request,"parent/student/studentdetails.html")
-=======
 from django.shortcuts import render
 from .models import *
 
@@ -82,4 +42,3 @@ def studentDetails(request):
 
 def editprofile(request):
     return render(request,"parent/editprofile.html")
->>>>>>> 12ad07180b029b0e5f575dbfcfd07ac81857ab1d
